7ff1909a617a5db59c83db7dceb79aadb15869ed
Wizehunt <kriszt4@gmail.com>
2018-11-06 12:57:13 +0100

    DB name extracted0

--- ReminderWear/src/Tasker/SQLitePersistor.ts
+++ ReminderWear/src/Tasker/SQLitePersistor.ts
@@ -22,18 +22,20 @@ export class SQLitePersistor {
   private db: SQLiteObject;
   private databaseReady: BehaviorSubject<boolean>;
 
+  private static DB_CONFIG = {
+    name: 'remindwear.db',
+    location: 'default'
+  };
+
   private constructor(sqlite: SQLite) {
     this.sqlite = sqlite;
     this.databaseReady = new BehaviorSubject(false);
-    this.sqlite.create({
-      name: 'remindwear.db',
-      location: 'default'
-    })
+    this.sqlite.create(SQLitePersistor.DB_CONFIG)
       .then((db: SQLiteObject) => {
         this.db = db;
-        this.createDBs();
+        this.createDBs(true);
       })
-      .catch(e => console.log('Erreur dans l\'ouverture de la BDD:' + e));
+      .catch(e => console.log('Erreur dans l\'ouverture de la BDD:' , e));
 
   }
 
@@ -77,10 +79,6 @@ export class SQLitePersistor {
     const tasks = SQLitePersistor.getInstance().loadTasksFromDB();
     const sportTasks = SQLitePersistor.getInstance().loadSportTasksFromDB();
 
-    // console.log('ajout des categories');
-    // console.log('les categories préchargées sont ', JSON.stringify(Tasker.getListCategories()));
-    // console.log('les catégories extraites sont ', JSON.stringify(categories));
-
     Tasker.getListCategories().forEach(value => categories.push(value));
     Tasker.setListCategories( categories );
     // console.log('les categories resultantes sont ', JSON.stringify(Tasker.getListCategories()));
@@ -111,59 +109,17 @@ export class SQLitePersistor {
     categories.forEach(function(cat: Category)  {
       if (cat.getName() !== Tasker.CATEGORY_NONE_TAG && cat.getName() !== Tasker.CATEGORY_SPORT_TAG) {
         db.executeSql('INSERT INTO `Categories` (name, icon, color) VALUES (?, ?, ?)', [
-          // cat.getID(),
           cat.getName(),
           cat.getIcon(),
           cat.getColor()
-          // 'example en dur', 'ios.alarm', '#FFFFFF'
         ]).then(res => {
-          console.log('retour de l\'insertion de la cat "' + cat.getName() + '" : ', JSON.stringify(res));
           cat.setID(res.insertId);
-          console.log('Verification de l\'insert');
-          db.executeSql('SELECT * FROM `Categories`', [])
-            .then(res2 => {
-              console.log('returned :', JSON.stringify(res2.rows), 'resultats');
-              console.log('row n° insertId : ', JSON.stringify(res2.rows.item(res.insertId)));
-              console.log('row n° 0 : ', JSON.stringify(res2.rows.item(0)));
-            })
-            .catch(e => console.log('erreur de lecture', JSON.stringify(e)));
         }).catch(e => {
-          console.log('erreur d\'insertion : ');
-          console.log(JSON.stringify(e));
+          console.log('erreur d\'insertion : ', JSON.stringify(e));
         });
-
-        /////////////
-        // db.executeSql('SELECT count(*) AS mycount FROM Categories', [])
-        //   .then(rs => console.log('Record count (expected to be 2): ' + rs.rows.item(0).mycount))
-        //   .catch(error => console.log('Error SQL statement', error.message));
       }
     });
 
-
-    // this.db.transaction(function(tx) {
-    //     categories.forEach(function(cat: Category)  {
-    //       if (cat.getName() !== Tasker.CATEGORY_NONE_TAG && cat.getName() !== Tasker.CATEGORY_SPORT_TAG) {
-    //         tx.executeSql('INSERT INTO Categories VALUES (?, ?, ?)', [
-    //           cat.getID(),
-    //           cat.getName(),
-    //           cat.getColor()
-    //         ]);
-    //       }
-    //     });
-    //   }
-    // ).catch(error => {
-    //   console.log('une erreur est survenue dans l\'insertion : ');
-    //   console.log(JSON.stringify(error));
-    // });
-
-    // this.sqlite.create({
-    //   name: 'remindwear.db',
-    //   location: 'default'
-    // })
-    //   .then((db: SQLiteObject) => {
-    //
-    //   })
-    //   .catch(e => console.log(e));
   }
 
   private saveSportTasksToDB(tasks: SportTask[]): void {
@@ -172,22 +128,16 @@ export class SQLitePersistor {
 
   private loadCategoriesFromDB(): Category[] {
     const categories: Category[] = [];
-    console.log('loadCategoriesFromDB');
-    // this.db.transaction(function(tx) {
-    //   tx.executeSql('SELECT count(*) AS mycount FROM Categories', [], function(rs) {
-    //     console.log('SELECT returned ', rs.rows);
-    //     console.log(JSON.stringify(rs));
-    //     console.log(rs.rows);
-    //     console.log('done reading SELECT');
-    //     // console.log('Record count (expected to be 2): ' + rs.rows.item(0).mycount);
-    //   }, function(error) {
-    //     console.log('SELECT error: ' + error.message);
-    //   });
-    // });
 
-    console.log('loadCategoriesFromDB : done, extracted categories : ');
-    console.log(JSON.stringify(categories));
-
+    this.db.executeSql('SELECT * FROM `Categories`', [])
+      .then(res => {
+        console.log('returned :', res.rows.length , 'resultats');
+        for (let i = 0; i < res.rows.length; i++) {
+          const sqlCat = res.rows.item(i);
+          categories.push( new Category(sqlCat.name, sqlCat.icon, sqlCat.color));
+        }
+      })
+      .catch(e => console.log('erreur de lecture', JSON.stringify(e)));
     return categories;
   }
 
@@ -200,31 +150,37 @@ export class SQLitePersistor {
   }
 
   private truncateDBs(): void {
-    console.log('truncate DBs : '),
-    this.db.executeSql('DROP TABLE IF EXISTS HeartRate')
-      .then(() => console.log('HeartRate Truncated'))
-      .catch(e => console.log('error truncating HeartRate: ', JSON.stringify(e)));
+    console.log('truncate DBs : ');
+    this.sqlite.deleteDatabase(SQLitePersistor.DB_CONFIG);
+    this.sqlite.create(SQLitePersistor.DB_CONFIG);
+    // this.db.executeSql('DELETE FROM HeartRate WHERE SportTaskID IS NOT NULL')
+    //   .then(() => console.log('HeartRate Truncated'))
+    //   .catch(e => console.log('error truncating HeartRate: ', JSON.stringify(e)));
+    //
+    // this.db.executeSql('DELETE FROM SportTasks')
+    //   .then(() => console.log('SportTasks Truncated'))
+    //   .catch(e => console.log('error truncating SportTasks : ', JSON.stringify(e)));
+    //
+    // this.db.executeSql('DROP TABLE Coordinates')
+    //   .then(() => console.log('Coordinates Truncated'))
+    //   .catch(e => console.log('error truncating Coordinates: ', JSON.stringify(e)));
+    //
+    // this.db.executeSql('DROP TABLE Tasks')
+    //   .then(() => console.log('Tasks Truncated'))
+    //   .catch(e => console.log('error truncating Tasks : ', JSON.stringify(e)));
 
-    this.db.executeSql('DELETE FROM SportTasks')
-      .then(() => console.log('SportTasks Truncated'))
-      .catch(e => console.log('error truncating SportTasks : ', JSON.stringify(e)));
+      // this.db.executeSql('DELETE FROM Categories WHERE ID IS NOT NULL')
+      //   .then(() => console.log('Categories Truncated'))
+      //   .catch(e => console.log('error dropping Categories: ', JSON.stringify(e)));
 
-    this.db.executeSql('DROP TABLE Coordinates')
-      .then(() => console.log('Coordinates Truncated'))
-      .catch(e => console.log('error truncating Coordinates: ', JSON.stringify(e)));
+    // this.db.executeSql('DROP TABLE Categories')
+    //   .then(() => console.log('CategoriesTruncated'))
+    //   .catch(e => console.log('error dropping Categories: ', JSON.stringify(e)));
 
-    this.db.executeSql('DROP TABLE Tasks')
-      .then(() => console.log('Tasks Truncated'))
-      .catch(e => console.log('error truncating Tasks : ', JSON.stringify(e)));
-
-    this.db.executeSql('DROP TABLE Categories')
-      .then(() => console.log('CategoriesTruncated'))
-      .catch(e => console.log('error truncating Categories: ', JSON.stringify(e)));
-
-    this.createDBs();
+    this.createDBs(false);
   }
 
-  private createDBs() {
+  private createDBs(notify: boolean) {
     this.db.transaction(function(tx) {
         tx.executeSql('CREATE TABLE IF NOT EXISTS Categories (' +
           'ID INTEGER PRIMARY KEY, ' +
@@ -268,7 +224,9 @@ export class SQLitePersistor {
       .catch(error => console.log(error))
       .then( value => {
         console.log('done creating the DBs : ', value);
-        this.databaseReady.next(true);
+        if (notify) {
+          this.databaseReady.next(true);
+        }
       } );
   }
 }
